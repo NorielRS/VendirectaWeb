7b6c16399420a33ee57eaee5257d092e95508570
NorielRS <norielrs04@gmail.com>
2025-08-09 18:37:17 +0800

    created cart.php

---
+++ addToCartFunctions.php
@@ -0,0 +1,12 @@
+<?php
+    class AddToCartFunctions extends UserShoppingFunction {
+
+        public function __construct() {
+        parent::__construct(); // Calls ParentClass constructor
+        // Child initialization
+    }
+
+
+
+    }
+?>
---
+++ cart.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'userShoppingFunction.php';
+$id = $_GET['id'];
+$singleProduct = $store->getSingleProduct($id);
+
+// Example product data (replace with your database query)
+$price = $singleProduct['price']; // price per product
+// $stocks_bought = $_GET['stocks_bought'];   // number of stocks available
+
+// $totalCost = $price * $stocks_bought;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cart</title>
+</head>
+<body>
+    <h2>Cart</h2>
+   
+    <h3>Your Product <?php echo $singleProduct['product_name'];; ?> has been ordered</h3>
+
+</body>
+</html>
